c96341f857b32672172c9c0263c5a21604167098
nekonoko114 <yoshua0826@gmail.com>
2026-03-26 07:52:39 +0900

    fix: update SkiaEffectsShowcase duration to 1800 to show all 10 effects

--- src/Root.tsx
+++ src/Root.tsx
@@ -884,7 +884,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="SKIA-EFFECTS-SHOWCASE"
         component={SkiaEffectsShowcase}
-        durationInFrames={900}
+        durationInFrames={1800}
         fps={30}
         width={1920}
         height={1080}
